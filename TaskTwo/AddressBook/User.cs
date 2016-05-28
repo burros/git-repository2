8a96b63b0cb4f367889f943697b9776812e3fae1
burros <957267@gmail.com>
2016-05-28 20:04:22 +0300

    task two result commit

---
+++ TaskTwo/AddressBook/User.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAddressBook
+{
+    public enum Gender
+    {
+        Male, Famale
+    }
+    public class User
+    {
+        private readonly DateTime _timeAdded;
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public DateTime TimeAdded
+        {
+            get { return _timeAdded; }
+        }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public Gender Gender { get; set; }
+        public string Email { get; set; }
+
+        // first name, second name, email for personal identification user
+        // i use it when i will be invoke method AddUser and RemoveUser
+        public User(string firstName, string secondName, string email)
+        {
+            FirstName = firstName;
+            SecondName = secondName;
+            Email = email;
+            _timeAdded = DateTime.Now;
+        }
+        public User(string firstName, string secondName, string email, DateTime birthdate, string city, string address, string phoneNumber, Gender gender)
+        {
+            FirstName = firstName;
+            SecondName = secondName;
+            Email = email;
+            Birthdate = birthdate;
+            City = city;
+            Address = address;
+            PhoneNumber = phoneNumber;
+            _timeAdded = DateTime.Now;
+        }
+    }
+
+}
---
+++ TaskTwo/TaskTwo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyAddressBook;
+using MyLogger;
+
+namespace TaskTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook book = new AddressBook();
+            // change class ConsoleLogger to FileLogger for write in the log file
+            ILogger logger = ConsoleLogger.Instance;
+            book.UserAdded += logger.Info;
+            book.UserRemoved += logger.Info;
+            book.UserNotAdded += logger.Warning;
+            book.UserNotRemoved += logger.Warning;
+
+
+            book.AddUser(new User("Joey", "Tribbiani", "jtr@mail.com", new DateTime(1980, 01, 10), "New York", "Bedford 90", "+44 20 7946 0213", Gender.Male));
+            book.AddUser(new User("Chandler", "Bing", "chb@mail.com", new DateTime(1980, 01, 10), "New York", "Bedford 90", "+44 20 7946 0213", Gender.Male));
+            book.AddUser(new User("Monica", "Geller", "moge@mail.com", new DateTime(1980, 01, 10), "New York", "Bedford 90", "+44 20 7946 0213", Gender.Famale));
+            book.AddUser(new User("Rachel", "Green", "rgr@mail.com", new DateTime(1980, 01, 10), "New York", "Bedford 90", "+44 20 7946 0213", Gender.Famale));
+            book.AddUser(new User("Phoebe", "Buffay", "phb@mail.com", new DateTime(1980, 01, 10), "New York", "Bedford 90", "+44 20 7946 0213", Gender.Famale));
+            book.AddUser(new User("Ross", "Geller", "rg@mail.com", new DateTime(1980, 01, 10), "New York", "Bedford 90", "+44 20 7946 0213", Gender.Male));
+            book.AddUser(new User("Ross", "Geller", "rg@mail.com", new DateTime(1980, 01, 10), "New York", "Bedford 90", "+44 20 7946 0213", Gender.Male));
+            book.RemoveUser("Joey", "Tribbiani", "jtr@mail.com");
+            book.RemoveUser("David", "Crane", "dcr@mail.com");
+
+            //show data from log file 
+            //FileLogger fileLogger = (FileLogger)logger;
+            //fileLogger.ShowLogMessages();
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
